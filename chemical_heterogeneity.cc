d77e3c1688b3369255c642fddba07458b47007df
Martina Monaco <67927796+martinamonaco@users.noreply.github.com>
2022-12-22 09:11:43 -0500

    Added comment

--- chemical_heterogeneity.cc
+++ chemical_heterogeneity.cc
@@ -16,6 +16,7 @@
   You should have received a copy of the GNU General Public License
   along with ASPECT; see the file LICENSE.  If not see
   <http://www.gnu.org/licenses/>.
+  Adding comment
 */
 
 #include <aspect/initial_composition/interface.h>
