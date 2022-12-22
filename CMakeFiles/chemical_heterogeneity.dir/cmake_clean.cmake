508aa09c4d5b68c72ea238ec64f36eada96909a9
Martina Monaco <67927796+martinamonaco@users.noreply.github.com>
2022-12-22 09:09:47 -0500

    Initializing repo

---
+++ CMakeFiles/chemical_heterogeneity.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chemical_heterogeneity.dir/chemical_heterogeneity.cc.o"
+  "CMakeFiles/chemical_heterogeneity.dir/chemical_heterogeneity.cc.o.d"
+  "libchemical_heterogeneity.pdb"
+  "libchemical_heterogeneity.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chemical_heterogeneity.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
